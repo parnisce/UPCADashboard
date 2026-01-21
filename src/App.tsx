48f60fa80dcc22667de39545cb34352ced176adc
Cyryl Bitangcol <cyryl.bitangcol@gmail.com>
2026-01-21 15:02:21 +0800

    Fix App.tsx syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,52 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useEffect } from "react";
-import { AppLayout } from './layouts/AppLayout';
-import { AdminLayout } from './layouts/AdminLayout';
-import { Dashboard } from './pages/Dashboard';
-import { OrdersPage } from './pages/OrdersPage';
-import { PropertiesPage } from './pages/PropertiesPage';
-import { LoginPage } from './pages/LoginPage';
+import { AppLayout } from "./layouts/AppLayout";
+import { AdminLayout } from "./layouts/AdminLayout";
 
-import { CreateOrderPage } from './pages/CreateOrderPage';
-import { AddPropertyPage } from './pages/AddPropertyPage';
-import { DeliverablesPage } from './pages/DeliverablesPage';
+import { Dashboard } from "./pages/Dashboard";
+import { OrdersPage } from "./pages/OrdersPage";
+import { PropertiesPage } from "./pages/PropertiesPage";
+import { LoginPage } from "./pages/LoginPage";
 
-import { AccountPage } from './pages/AccountPage';
-import { EditPropertyPage } from './pages/EditPropertyPage';
-import { OrderDetailPage } from './pages/OrderDetailPage';
-import { BillingPage } from './pages/BillingPage';
-import { BookingsPage } from './pages/BookingsPage';
-import { PaymentMethodsPage } from './pages/PaymentMethodsPage';
-import { SupportPage } from './pages/SupportPage';
+import { CreateOrderPage } from "./pages/CreateOrderPage";
+import { AddPropertyPage } from "./pages/AddPropertyPage";
+import { DeliverablesPage } from "./pages/DeliverablesPage";
+
+import { AccountPage } from "./pages/AccountPage";
+import { EditPropertyPage } from "./pages/EditPropertyPage";
+import { OrderDetailPage } from "./pages/OrderDetailPage";
+import { BillingPage } from "./pages/BillingPage";
+import { BookingsPage } from "./pages/BookingsPage";
+import { PaymentMethodsPage } from "./pages/PaymentMethodsPage";
+import { SupportPage } from "./pages/SupportPage";
 
 // Admin Pages
-import { AdminDashboard } from './pages/admin/AdminDashboard';
-import { AdminServicesPage } from './pages/admin/AdminServicesPage';
-import { AdminBookingsPage } from './pages/admin/AdminBookingsPage';
-import { AdminOrdersPage } from './pages/admin/AdminOrdersPage';
-import { AdminMessagesPage } from './pages/admin/AdminMessagesPage';
-import { AdminOrderDetailPage } from './pages/admin/AdminOrderDetailPage';
+import { AdminDashboard } from "./pages/admin/AdminDashboard";
+import { AdminServicesPage } from "./pages/admin/AdminServicesPage";
+import { AdminBookingsPage } from "./pages/admin/AdminBookingsPage";
+import { AdminOrdersPage } from "./pages/admin/AdminOrdersPage";
+import { AdminMessagesPage } from "./pages/admin/AdminMessagesPage";
+import { AdminOrderDetailPage } from "./pages/admin/AdminOrderDetailPage";
 
-import { UserProvider } from './contexts/UserContext';
-
-import { NotificationProvider } from './contexts/NotificationContext';
+import { UserProvider } from "./contexts/UserContext";
+import { NotificationProvider } from "./contexts/NotificationContext";
 
 function App() {
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const redirect = params.get("redirect");
-
-    if (redirect) {
-      const base = "/UPCADashboard";
-      const clean = redirect.startsWith("/") ? redirect : "/" + redirect;
-      window.history.replaceState({}, "", base + clean);
-    }
-  }, []);
-
   return (
     <UserProvider>
       <NotificationProvider>
         <Routes>
           <Route index element={<Navigate to="/dashboard" replace />} />
           <Route path="/login" element={<LoginPage />} />
+
+          {/* Admin Routes */}
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<AdminDashboard />} />
+            <Route path="services" element={<AdminServicesPage />} />
+            <Route path="bookings" element={<AdminBookingsPage />} />
+            <Route path="orders" element={<AdminOrdersPage />} />
+            <Route path="orders/:id" element={<AdminOrderDetailPage />} />
+            <Route path="messages" element={<AdminMessagesPage />} />
+          </Route>
 
           {/* Client Routes */}
           <Route element={<AppLayout />}>
@@ -65,16 +63,6 @@ function App() {
             <Route path="/billing" element={<BillingPage />} />
             <Route path="/billing/payment-methods" element={<PaymentMethodsPage />} />
             <Route path="/support" element={<SupportPage />} />
-          </Route>
-
-          {/* Admin Routes */}
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<AdminDashboard />} />
-            <Route path="services" element={<AdminServicesPage />} />
-            <Route path="bookings" element={<AdminBookingsPage />} />
-            <Route path="orders" element={<AdminOrdersPage />} />
-            <Route path="orders/:id" element={<AdminOrderDetailPage />} />
-            <Route path="messages" element={<AdminMessagesPage />} />
           </Route>
         </Routes>
       </NotificationProvider>
